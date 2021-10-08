f5678a453e783e11901b95c353f74e35b3ca4640
Vince Llauderes <vincent_llauderes@manulife.ca>
2021-10-08 15:10:31 +0800

    rolling update deployment

--- deployments/node-app/v1/server.js
+++ deployments/node-app/v1/server.js
@@ -1,9 +1,9 @@
-const http = require('http'),
-      os = require('os');
+const http = require("http"),
+  os = require("os");
 
 console.log("Node v1 server starting...");
 
-var handler = function(request, response) {
+var handler = function (request, response) {
   console.log("Request received from: " + request.connection.remoteAddress);
   response.writeHead(200);
   response.end("Node v1 running in a pod: " + os.hostname() + "\n");
